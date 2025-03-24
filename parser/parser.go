7190ae92da747e52cdc5877ca7b431e417b765de
flightman69 <flightman69420@gmail.com>
2025-03-24 15:33:50 +0530

    started with parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/flightman69/go-interpreter/ast"
 	"github.com/flightman69/go-interpreter/lexer"
 	"github.com/flightman69/go-interpreter/token"
@@ -11,10 +13,15 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// Read two token, so curToken and peekToken are both set
 	p.nextToken()
@@ -89,4 +96,14 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 	} else {
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
